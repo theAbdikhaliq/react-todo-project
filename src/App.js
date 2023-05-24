6abb2a578b0c1506abe5c61c045e55c83fea5894
Abdikhaliq Mohamoud <mo.abdikhaliq@gmail.com>
2023-05-24 12:04:57 +0300

    Add button functionality implemented.

--- src/App.js
+++ src/App.js
@@ -2,63 +2,48 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
- const [todo, setTodo] = useState([
-  { id: 1, task: "task-2" },
-  { id: 2, task: "task-3" },
- ]);
- const [input, setInput] = useState("");
+    const [newTask, setNewTask] = useState()
+    const [todos, setTodos] = useState([
+        {id: 1, task: "Task 1"},
+        {id: 2, task: "Task 2"}
+    ])
 
- function handleOnChange(event) {
-  setInput(event.target.value);
- }
+    //track changes on <input>
+    const handleOnChange = (e) => {
+        setNewTask(e.target.value)
+    }
 
- function handleOnClick() {
-  const todos = {
-   id: todo.length === 0 ? 1 : todo[todo.length - 1].id + 1,
-   task: input,
-  };
-  setTodo([...todo, todos]);
- }
+    //add task to todos
+    const addOnClick = () => {
+        const id = todos[todos.length - 1].id + 1
+        const todo = {
+            id: id,
+            task: newTask
+        }
+        setTodos([...todos, todo])
+    }
 
- const handleOnDelete = (value) => {
-  const deletedItem = todo.filter((filtered, index) => {
-   return filtered.id !== value;
-  });
-  console.log(deletedItem);
-  setTodo(deletedItem);
- };
 
- // ["task-6","task-7","task-8"]
- return (
-  <div className="app">
-   <h1>Todo List</h1>
-   <div className="todo-input">
-    <input
-     type="text"
-     placeholder="Add a new task..."
-     onChange={handleOnChange}
-    />
-    <button onClick={handleOnClick}>Add</button>
-   </div>
-   <ul className="todo-list">
-    {todo.map(function (value, index) {
-     return (
-      <li className="">
-       {console.log(value)}
-       {value.id}. {value.task}
-       <div className="button-group">
-        <button className="complete-button">Complete</button>
-        <button
-         className="delete-button"
-         onClick={() => handleOnDelete(value.id)}>
-         Delete
-        </button>
-       </div>
-      </li>
-     );
-    })}
-   </ul>
-  </div>
+    return (
+        <div className="app">
+            <h1>Todo List</h1>
+            <div className="todo-input">
+                <input type="text" placeholder="Add a new task..." onChange={handleOnChange}/>
+                <button onClick={addOnClick}>Add</button>
+            </div>
+            
+            <ul className="todo-list">
+                {todos.map(todo => (
+                    <li className="">
+                        {todo.id}. {todo.task}
+                        <div className="button-group">
+                            <button className="complete-button">Complete</button>
+                            <button className="delete-button">Delete</button>
+                        </div>
+                    </li>
+                ))}
+            </ul>
+        </div>
  );
 }
 
