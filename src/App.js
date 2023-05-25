12db52c6f8e9a8f7b8f0d33525be39a1a68aa534
Abdikhaliq Mohamoud <mo.abdikhaliq@gmail.com>
2023-05-25 11:24:25 +0300

    Complete button functionality implemented.

--- src/App.js
+++ src/App.js
@@ -4,8 +4,7 @@ import "./App.css";
 function App() {
     const [newTask, setNewTask] = useState()
     const [todos, setTodos] = useState([
-        {id: 1, task: "Task 1"},
-        {id: 2, task: "Task 2"}
+        {id: 1, task: 'Your first task.'}
     ])
 
     //track changes on <input>
@@ -18,21 +17,30 @@ function App() {
         const id = todos.id === 0 ? 1 : todos[todos.length - 1].id + 1
         const todo = {
             id: id,
-            task: newTask
+            task: newTask,
+            completed: false
         }
         setTodos([...todos, todo])
     }
 
     //delete task
     const deleteOnClick = (id) => {
-        const remainedTasks = todos.filter(todo => todo.id !== id)
-        setTodos(remainedTasks)
+        const remainedTodos = todos.filter(todo => todo.id !== id)
+        setTodos(remainedTodos)
+    }
+
+    //complete task
+    const completeOnClick = (id) => {
+        setTodos(todos.map(todo => (
+            todo.id === id ? {...todo, completed: !todo.completed} : todo
+        )))
     }
 
 
     return (
         <div className="app">
             <h1>Todo List</h1>
+            <p style={{textAlign: 'right'}}>See on <a href="https://github.com/theAbdikhaliq/react-todo-project" target="_blank">Github</a></p>
             <div className="todo-input">
                 <input type="text" placeholder="Add a new task..." onChange={handleOnChange}/>
                 <button onClick={addOnClick}>Add</button>
@@ -40,15 +48,17 @@ function App() {
             
             <ul className="todo-list">
                 {todos.map(todo => (
-                    <li className="">
+                    <li className={todo.completed ? "completed" : ""}>
                         {todo.id}. {todo.task}
                         <div className="button-group">
-                            <button className="complete-button">Complete</button>
+                            <button className="complete-button" onClick={() => completeOnClick(todo.id)}>Complete</button>
                             <button className="delete-button" onClick={() => deleteOnClick(todo.id)}>Delete</button>
                         </div>
                     </li>
                 ))}
             </ul>
+            {/* <div style={{marginBottom: "5px"}}>helye</div>
+            <footer style={{marginBottom: '5px'}}>Copyright</footer> */}
         </div>
  );
 }
